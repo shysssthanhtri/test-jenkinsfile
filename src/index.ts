60aa559bdf987c053e87580537b5d5332937f84c
tri.nguyen <shyssthanhtri@gmail.com>
2022-11-12 20:56:46 +0700

    sayHello

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello world')
+import { sayHello } from './services/say-hello/say-hello'
+
+const main = (): void => {
+  const greeting = sayHello('Tri')
+  console.log(greeting)
+}
+
+main()
---
+++ src/services/say-hello/say-hello.ts
@@ -0,0 +1,3 @@
+export const sayHello = (name: string): string => (
+    `Hello ${name}`
+)
